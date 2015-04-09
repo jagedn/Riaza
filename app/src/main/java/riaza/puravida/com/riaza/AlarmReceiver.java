5733b714822518f9eed08590aa7db389f98c064d
Jorge Aguilera <jagedn@gmail.com>
2015-04-09 10:45:49 +0200

    Initial commit

---
+++ app/src/main/java/riaza/puravida/com/riaza/AlarmReceiver.java
@@ -0,0 +1,60 @@
+package riaza.puravida.com.riaza;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import java.util.Calendar;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    public AlarmReceiver() {
+    }
+
+    static PendingIntent sender;
+
+    @Override
+    public void onReceive(Context context, Intent i) {
+        Intent intent = new Intent(context,TakePictureActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Bundle bundle = new Bundle();
+        bundle.putBoolean("take",true);
+        intent.putExtras(bundle);
+
+        context.startActivity(intent);
+    }
+
+    static final int WHAT = 192837;
+
+    public static void scheduleNext(Activity activity){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        int pHour = Integer.parseInt(prefs.getString("sync_frequency","-1"));
+        if( pHour != -1) {
+            Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            if( pHour <= hour ){
+                c.add(Calendar.HOUR, 24-hour+pHour);
+            }else{
+                c.add(Calendar.HOUR, pHour-hour);
+            }
+
+            Intent intent = new Intent(activity, AlarmReceiver.class);
+            sender = PendingIntent.getBroadcast(activity, WHAT, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            AlarmManager am = (AlarmManager) activity.getSystemService(Context.ALARM_SERVICE);
+            am.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), sender);
+        }
+
+    }
+
+    public static void cancelSchedule(Activity activity){
+        if( sender != null ) {
+            AlarmManager am = (AlarmManager) activity.getSystemService(Context.ALARM_SERVICE);
+            am.cancel(sender);
+        }
+    }
+}
---
+++ app/src/main/java/riaza/puravida/com/riaza/TakePictureActivity.java
@@ -0,0 +1,141 @@
+package riaza.puravida.com.riaza;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class TakePictureActivity extends Activity implements SurfaceHolder.Callback {
+    private static final String TAG = "takepicture";
+
+    private ImageView mImageView;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mHolder;
+
+    static private Camera mCamera;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_picture);
+
+        boolean take = getIntent().getExtras().getBoolean("take");
+        if( take ) {
+            mImageView = (ImageView) findViewById(R.id.imageView);
+            mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+            mHolder = mSurfaceView.getHolder();
+            mHolder.addCallback(this);
+        }else{
+            AlarmReceiver.cancelSchedule(this);
+            finish();
+        }
+    }
+
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h)
+    {
+        mCamera.startPreview();
+
+        Camera.PictureCallback mCall = new Camera.PictureCallback()
+        {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera)
+            {
+                Uri uriTarget = getContentResolver().insert//(Media.EXTERNAL_CONTENT_URI, image);
+                        (MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new ContentValues());
+
+                OutputStream imageFileOS;
+                try {
+                    imageFileOS = getContentResolver().openOutputStream(uriTarget);
+                    imageFileOS.write(data);
+                    imageFileOS.flush();
+                    imageFileOS.close();
+
+                    Toast.makeText(TakePictureActivity.this,
+                            "Image saved: " + uriTarget.toString(), Toast.LENGTH_LONG).show();
+                }
+                catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //mCamera.startPreview();
+
+                Bitmap bmp = BitmapFactory.decodeByteArray (data, 0, data.length);
+                mImageView.setImageBitmap(bmp);
+
+                AlarmReceiver.scheduleNext(TakePictureActivity.this);
+                TakePictureActivity.this.finish();
+            }
+        };
+
+        mCamera.takePicture(null, null, mCall);
+    }
+
+    int getFrontCameraId() {
+        CameraInfo ci = new CameraInfo();
+        for (int i = 0 ; i < Camera.getNumberOfCameras(); i++) {
+            Camera.getCameraInfo(i, ci);
+            if (ci.facing == CameraInfo.CAMERA_FACING_FRONT) return i;
+        }
+        return -1; // No front-facing camera found
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+        int index = getFrontCameraId();
+        if (index == -1){
+            Toast.makeText(getApplicationContext(), "No front camera", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            mCamera = Camera.open(index);
+            Toast.makeText(getApplicationContext(), "With front camera", Toast.LENGTH_LONG).show();
+            try {
+                mCamera.setPreviewDisplay(holder);
+
+            } catch (IOException exception) {
+                mCamera.release();
+                mCamera = null;
+            }
+        }
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
+    }
+}
